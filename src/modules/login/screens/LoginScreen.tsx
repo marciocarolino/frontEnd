286eb413ffa1ad9346ebc88a7c4532b5677df007
Marcio Carolino <marciocarolino@gmail.com>
2023-06-16 17:30:11 -0300

    feat: insert state in login

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Button from '../../../shared/buttons/button/button';
 import Input from '../../../shared/inputs/input/Input';
 import {
@@ -10,6 +12,21 @@ import {
 } from '../styles/loginScreen.styles';
 
 const LoginScreen = () => {
+  const [username, setUSerName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUserName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUSerName(event.target.value);
+  };
+
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+    alert(`${username}, ${password}`);
+  };
+
   return (
     <>
       <ContainerLoginScreen>
@@ -18,15 +35,26 @@ const LoginScreen = () => {
             <TitleLogin level={2} type="secondary">
               Login
             </TitleLogin>
-            <Input title="Usuário" />
-            <Input title="Password" type="password" />
-            <Button type="primary" margin="64px 0px 16px 0px">
+            <Input
+              title="Usuário"
+              margin="32px 0px 0px"
+              onChange={handleUserName}
+              value={username}
+            />
+            <Input
+              title="Password"
+              type="password"
+              margin="32px 0px 0px"
+              onChange={handlePassword}
+              value={password}
+            />
+            <Button type="primary" margin="64px 0px 16px 0px" onClick={handleLogin}>
               ENTRAR
             </Button>
           </LimitedContainer>
         </ContainerLogin>
-        <BackgroundImage />
       </ContainerLoginScreen>
+      <BackgroundImage />
     </>
   );
 };
